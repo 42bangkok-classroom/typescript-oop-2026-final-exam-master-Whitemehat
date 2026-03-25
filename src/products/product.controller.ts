526e2fa703a636bc1ca50463f307dc6eb782e766
event <event@z2t13c1.42bangkok.com>
2026-03-25 10:32:05 +0700

    9th

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, UseFilters, UseInterceptors } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { ApiExceptionFilter } from 'src/common/api-exception.filter';
 import { ResponseInterceptor } from 'src/common/api-interceptor';
 @Controller('products')
 export class ProductController {
   constructor(private readonly ProductService: ProductService) {}
   @UseInterceptors(ResponseInterceptor)
-  @UseFilters(ApiExceptionFilter)
   @Get()
   findAll() {
     return {

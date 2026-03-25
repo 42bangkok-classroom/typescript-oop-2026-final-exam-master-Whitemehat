b4af9ab6e32d8310ece67c58e710bb4a7a509261
event <event@z2t13c1.42bangkok.com>
2026-03-25 11:10:41 +0700

    16th

--- src/purchases/purchase.controller.ts
+++ src/purchases/purchase.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 
 @Controller('purchases')
 export class PurchaseController {
-  constructor(private readonly ProductService: PurchaseService) {}
+  constructor(private readonly PurchaseService: PurchaseService) {}
   @Get()
   findAll() {
     return {
       success: true,
-      data: this.ProductService.findAll(),
+      data: this.PurchaseService.findAll(),
       message: 'Fetched purchases successfully',
     };
+  }
+
+  @Get()
+  findOne(
+    @Query('customerName') customerName: string
+  ){
+    return {
+        success: true,
+        data: this.PurchaseService.findOne(customerName),
+        message: 'Fetched purchases successfully',
+      };
   }
 }
--- src/purchases/purchase.service.ts
+++ src/purchases/purchase.service.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { Purchase } from './purchase.interface';
+import { NotFoundException } from '@nestjs/common';
 
 const filepath = './data/purchases.json';
 export class PurchaseService {
@@ -7,5 +8,15 @@ export class PurchaseService {
     const rawData = fs.readFileSync(filepath, 'utf-8');
     const data = JSON.parse(rawData) as Purchase[];
     return data;
+  }
+
+  findOne(customerName: string): Purchase{
+    const rawData = fs.readFileSync(filepath , 'utf-8');
+    const data = JSON.parse(rawData) as Purchase[];
+    const customer = data.find(d => d.customerName === customerName);
+    if(!customer){
+        throw new NotFoundException()
+    }
+    return customer
   }
 }

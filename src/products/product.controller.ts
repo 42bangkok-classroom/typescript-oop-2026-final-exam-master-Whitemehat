3a6a9615ba1f1112fb30dceb3538d4c0fdb7a3ab
event <event@z2t13c1.42bangkok.com>
2026-03-25 09:44:34 +0700

    3rd

---
+++ src/products/product.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+@Controller('products')
+export class ProductController {
+    private readonly ProductService: ProductService
+    @Get()
+    findAll(){
+        return this.ProductService.findAll();
+    }
+}
